6feb4fb396b9bef0631a0f7dc1ab7653f9b36058
Felix Haardjana <fhardjana@gmail.com>
2026-03-31 13:37:45 -0400

    Added border, color, and center the box

--- frontend/app/components/TruckCard.jsx
+++ frontend/app/components/TruckCard.jsx
@@ -3,12 +3,16 @@ export default function TruckCard({truckName}){
     
     return (
         <>
-        <div className="border">
+        <div className="flex justify-center"> 
+            <div className="bg-blue-500 mb-4 p-4 w-5/12 rounded-4xl">
 
-            <h1 className="">{truckName}</h1> {/*This is the parameter for the food truck*/}
-            <hr/>
-            <p>This is the description for the food truck</p>
-        </div>        
+                <h1 className="">{truckName}</h1> {/*This is the parameter for the food truck*/}
+                <hr/>
+                <p>This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
+            </div>  
+        </div>      
         </>
     )
 }
+
+//name, type of food, open_status, location
