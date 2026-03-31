8b596bd23441524d9d8c2cd50107f7ad2184b618
Felix Haardjana <fhardjana@gmail.com>
2026-03-31 16:28:22 -0400

    made a section for icon and created a component for it

---
+++ frontend/app/components/IconCard.jsx
@@ -0,0 +1,5 @@
+export default function IconCard(){
+    return (
+        <p>hey</p>
+    )
+}
--- frontend/app/components/TruckCard.jsx
+++ frontend/app/components/TruckCard.jsx
@@ -1,10 +1,12 @@
+import IconCard from "./IconCard"
+
 export default function TruckCard({truckName}){
     // hi
     
     return (
         <>
         <div className="flex justify-center"> {/* Keep this for now so that the box is centered */}
-            <div className="bg-blue-500 mb-4 w-5/12 rounded-4xl border-4 m-5 overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300"> {/* This is to stylize the content inside the box */}
+            <div className="bg-blue-500 mb-4 w-4/12 rounded-4xl border-4 m-5 overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300"> {/* This is to stylize the content and the individual card */}
                 <div className="bg-amber-800 h-64 rounded-t-4xl"> {/*this is an image placeholder*/}
                 </div>
                 <div className="p-4">
@@ -13,6 +15,12 @@ export default function TruckCard({truckName}){
                     <hr/>
                     <p className="wrap-break-word p-4">This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
                 </div>  
+
+                    <hr/>
+                <div className="p-4"> {/* this is for the icons popup: location, contacts, some sample of the menu */}
+                    <IconCard></IconCard> {/* the deisgn of the icon will be in the IconCard component */}
+                    <p> Add icon here </p>
+                </div>
             </div>     
         </div> 
         </>
