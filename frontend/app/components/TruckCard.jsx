e24cc51850874e6a7f62c91ef0b2bbd11a227715
Nathan Cherny <natec3632@gmail.com>
2026-03-31 13:01:10 -0400

    adding component for TruckCard

---
+++ frontend/app/components/TruckCard.jsx
@@ -0,0 +1,11 @@
+export default function TruckCard(){
+    return (
+        <>
+        <div>
+            <h1>This is a cool card</h1>
+            <hr/>
+            <p>oh <i>yeah</i></p>
+        </div>        
+        </>
+    )
+}
--- frontend/app/trucks/page.js
+++ frontend/app/trucks/page.js
@@ -1,15 +1,18 @@
-import Link from "next/link";
+// imports from next
+
+// importing components
+import TruckCard from "../components/TruckCard";
 
 const trucks = ["halal", "teppanyaki", "bagel hut"];
 
-export default function TrucksPage() {
+export default function AllTrucksPage() {
   return (
     <div>
       <h1>All Trucks</h1>
       <ul>
         {trucks.map((truck) => (
           <li key={truck}>
-            <Link href={`/trucks/${truck}`}>{truck}</Link>
+            <a href={`/trucks/${truck}`}>{truck}</a>
           </li>
         ))}
       </ul>
