d297fa9295e260c5c64301579baacce3606f41dc
Felix Haardjana <fhardjana@gmail.com>
2026-03-31 13:44:06 -0400

    changed the div style

--- frontend/app/components/TruckCard.jsx
+++ frontend/app/components/TruckCard.jsx
@@ -3,10 +3,10 @@ export default function TruckCard({truckName}){
     
     return (
         <>
-        <div className="flex justify-center"> 
-            <div className="bg-blue-500 mb-4 p-4 w-5/12 rounded-4xl">
+        <div className="flex justify-center bg-blue-500 mb-4 p-4 w-5/12 rounded-4xl"> 
+            <div className="">
 
-                <h1 className="">{truckName}</h1> {/*This is the parameter for the food truck*/}
+                <h1 className="text-center">{truckName}</h1> {/*This is the parameter for the food truck*/}
                 <hr/>
                 <p>This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
             </div>  
