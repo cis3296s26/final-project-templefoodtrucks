73b5793b1efcba82b17ad40b7bf6e375c8710761
Felix Haardjana <fhardjana@gmail.com>
2026-03-31 15:59:42 -0400

    made the hover animation for the truck card component

--- frontend/app/components/TruckCard.jsx
+++ frontend/app/components/TruckCard.jsx
@@ -4,15 +4,14 @@ export default function TruckCard({truckName}){
     return (
         <>
         <div className="flex justify-center"> {/* Keep this for now so that the box is centered */}
-            <div className="bg-blue-500 mb-4 w-5/12 rounded-4xl border-4 m-5 overflow-hidden"> {/* This is to stylize the content inside the box */}
+            <div className="bg-blue-500 mb-4 w-5/12 rounded-4xl border-4 m-5 overflow-hidden hover:scale-105 hover:shadow-xl transition-all duration-300"> {/* This is to stylize the content inside the box */}
                 <div className="bg-amber-800 h-64 rounded-t-4xl"> {/*this is an image placeholder*/}
-
                 </div>
                 <div className="p-4">
 
                     <h1 className="text-center p-2">{truckName}</h1> {/*This is the parameter for the food truck*/}
                     <hr/>
-                    <p className="wrap-break-word">This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
+                    <p className="wrap-break-word p-4">This is the description for the food truck. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt odio quis cum commodi ullam harum velit atque culpa quod molestias nihil amet quam sapiente, exercitationem sit optio saepe aliquam cupiditate.</p>
                 </div>  
             </div>     
         </div> 
