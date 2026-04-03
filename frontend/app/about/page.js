5da9ade3a4b79685be2fe533295c73d93a1cf5dd
Avenue Goldberg <mdgoldberg911@gmail.com>
2026-04-02 20:10:59 -0400

    testing commit

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import { PageMain } from "../components/PageMain";
+import {PageMain} from '../components/PageMain';
 
 export default function Home() {
   return (
     <PageMain>
-      <h1 className="text-5xl font-semibold" >What is Temple Food Trucks?</h1>
-      <hr className="m-10 text-gray-500 w-11/12"></hr>
-      <h1 className="self-start"><i>A New Way To Find Food On Campus</i></h1>
+      <h1 className='text-5xl font-semibold' >What is Temple Food Trucks?</h1>
+      <hr className='m-10 text-gray-500 w-11/12'></hr>
+      <h1 className=''><i>A New Way To Find Food On Campus</i></h1>
+      <h1 className=''>Hello World!</h1>
     </PageMain>
   );
 }
