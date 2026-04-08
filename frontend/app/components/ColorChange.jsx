6fd56c0e7950b550af3a71e53eb01498638c1000
Felix Haardjana <fhardjana@gmail.com>
2026-04-07 22:48:32 -0400

    designed the animated background

---
+++ frontend/app/components/ColorChange.jsx
@@ -0,0 +1,13 @@
+export default function ColorChange(){
+    return(
+        <>
+        <div className="relative">
+                <div className="animate-glow absolute left-100 bottom-15 w-30 h-30"/>
+                <div className="animate-glow absolute right-70 w-45 h-35"/>
+                <div className="animate-glow absolute left-250 bottom-15 w-30 h-30"/>
+                <div className="animate-glow absolute left--250 top-80 right-130 mb-4 w-70 h-60"/>
+                <div className="animate-glow absolute left-150 ml-70 w-50 h-50"/>
+            </div>
+        </>
+    )
+}
--- frontend/app/components/SignUpDesign.jsx
+++ frontend/app/components/SignUpDesign.jsx
@@ -1,33 +1,39 @@
+import ColorChange from "./ColorChange";
+
 export default function SignUpDesign(){
     return(
         <>
-        <div className="flex justify-center m-4">
-            <div className="bg-blue-500 w-200 h-150 rounded-4xl p-12">
-                <h1 className="font-[Georgia] font-bold text-4xl">Sign Up</h1>
-                <div className="flex flex-wrap justify-center m-2 p-5 gap-7">
-                    <div className="bg-white w-120 h-10 rounded-4xl">
-                        <input placeholder="Full Name" required className="text-black focus:outline-none flex p-2 w-full"></input>
-                    </div>
+        <div className="flex justify-center relative m-4">
+            <div className="absolute top-0 left-0">
+                <ColorChange/>
+            </div>
+                <div className="relative bg-blue-500 w-200 h-150 rounded-4xl p-12 shadow-2xl shadow-cyan-500"> {/*Relative allows the circle to be hidden behind the blue box */}
+                    <h1 className="font-[Georgia] font-bold text-4xl">Sign Up</h1>
+                    <div className="flex flex-wrap justify-center m-2 p-5 gap-7">
+                        <div className="bg-white w-120 h-10 rounded-4xl">
+                            <input placeholder="Full Name" required className="text-black focus:outline-none flex p-2 w-full"></input>
+                        </div>
 
-                    <div className="bg-white w-120 h-10 rounded-4xl">
-                        <input type="email" placeholder="Email Address" required className="text-black focus:outline-none flex p-2 w-full"></input>
-                    </div>
+                        <div className="bg-white w-120 h-10 rounded-4xl">
+                            <input type="email" placeholder="Email Address" required className="text-black focus:outline-none flex p-2 w-full"></input>
+                        </div>
 
-                    <div className="bg-white w-120 h-10 rounded-4xl">
-                        <input type="password" placeholder="Password" required className="text-black focus:outline-none flex p-2 w-full"></input>
-                    </div>
+                        <div className="bg-white w-120 h-10 rounded-4xl">
+                            <input type="password" placeholder="Password" required className="text-black focus:outline-none flex p-2 w-full"></input>
+                        </div>
 
-                    <div className="bg-white w-120 h-10 rounded-4xl">
-                        <input type="password" placeholder="Confirm Password" required className="text-black focus:outline-none flex p-2 w-full"></input>
-                    </div>
+                        <div className="bg-white w-120 h-10 rounded-4xl">
+                            <input type="password" placeholder="Confirm Password" required className="text-black focus:outline-none flex p-2 w-full"></input>
+                        </div>
 
-                    <div className="bg-black w-120 h-15 m-4 p-2 rounded-4xl">
-                        <button className="text-white flex justify-center p-1 w-full rounded-2xl font-bold text-3xl hover:bg-white hover:text-black">Sign Up</button>
+                        <div className="bg-black w-120 h-15 m-4 p-2 rounded-4xl overflow-hidden">
+                            <button className="text-white flex justify-center p-1 w-full hover:bg-white hover:text-black">
+                                <p className="font-bold text-3xl">Sign Up</p>
+                                </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        
         </>
     )
 }
