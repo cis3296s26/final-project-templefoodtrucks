6e2ba33506f598f9ddcc57c94874728f6c71a6cd
ajnieves1 <tun29995@temple.edu>
2026-04-09 13:48:47 -0400

    Created tests within tests.py

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.gis.geos import Point
+from .models import FoodTruck
 
-# Create your tests here.
+# To run tests
+# Run: "docker compose exec web python manage.py test --keepdb" from root directory
+# What this does, is run tests against a test database within Neon. 
+class TruckTest(TestCase):
+    def setUp(self):
+        FoodTruck.objects.create(
+            name="Temple Creperie",
+            foodType="French",
+            priceRange="$",
+            location=Point(-75.153, 39.981)
+    )
+    
+    # Test function
+    # Checks if "Temple Creperie" was created and stored
+    def test_truck_creation(self):
+        truck = FoodTruck.objects.get(name="Temple Creperie")
+        self.assertEqual(truck.foodType, "French")
+        self.assertEqual(truck.priceRange, "$")
+
+    # Tests that the food truck is represented by a string
+    def test_string_representation(self):
+        truck = FoodTruck.objects.get(name="Temple Creperie")
+        self.assertEqual(str(truck), "Temple Creperie")
