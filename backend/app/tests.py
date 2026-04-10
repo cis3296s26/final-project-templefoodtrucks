31a51baae7865f876bc2307afb770d29efc7d665
Andrew Nieves <tun29995@temple.edu>
2026-04-10 01:00:44 -0400

    Fixed bugs within tests.py

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -5,13 +5,19 @@ from .models import FoodTruck
 # To run tests
 # Run: "docker compose exec web python manage.py test --keepdb" from root directory
 # What this does, is run tests against a test database within Neon. 
+# To destroy the old version of a test database, run this:
+# docker compose exec web python manage.py test
+
 class TruckTest(TestCase):
     def setUp(self):
         FoodTruck.objects.create(
             name="Temple Creperie",
             foodType="French",
             priceRange="$",
-            location=Point(-75.153, 39.981)
+            location=Point(-75.153, 39.981),
+            status='OPEN',
+            openingTime="09:00:00", # 9 AM
+            closingTime="21:00:00" # 9 PM
     )
     
     # Test function
@@ -20,6 +26,7 @@ class TruckTest(TestCase):
         truck = FoodTruck.objects.get(name="Temple Creperie")
         self.assertEqual(truck.foodType, "French")
         self.assertEqual(truck.priceRange, "$")
+        self.assertEqual(truck.status, 'OPEN')
 
     # Tests that the food truck is represented by a string
     def test_string_representation(self):
