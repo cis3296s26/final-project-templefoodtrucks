bcea8195b7cd157c3d0368fbec36729e07da0d4f
Felix Haardjana <fhardjana@gmail.com>
2026-04-13 23:40:52 -0400

    Add icons to the user and truck panels

--- frontend/app/components/MiniPopUpInfo.jsx
+++ frontend/app/components/MiniPopUpInfo.jsx
@@ -21,7 +21,7 @@ export default function MiniPopUpInfo(){
         <div className="animate-signupPop"> 
             <div className={`transition-opacity duration-700 ${loaded ? "opacity-100" : "opacity-0"}`}>
                 <div className={`transition-opacity duration-500 ${visible ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}`}>
-                    <div className="fixed w-150 h-100 bg-red-500 top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-2xl border-8 border-green-400 p-3">
+                    <div className="fixed w-150 h-100 scale-200 bg-red-500 top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-2xl border-8 border-green-400 p-3">
                         <h1 className="font-bold text-5xl m-5">FOOD TRUCK CUSTOMIZATION</h1>
                         <p className="text-3xl m-4">You will now be customizing your personal Food Truck</p>
                         <p className="text-3xl m-4">Have Fun!</p>
--- frontend/app/components/UserOrTruck.jsx
+++ frontend/app/components/UserOrTruck.jsx
@@ -1,10 +1,16 @@
 "use client"
 
 import { useRouter } from "next/navigation"
+import {
+    Truck,
+    User
+} from "lucide-react"
+import { useState } from "react";
 
 export default function UserOrTruck(){
 
     const router = useRouter();
+    const [userType, setUserType] = useState();
 
     function CustomerRouter(e){
         e.preventDefault();
@@ -20,10 +26,16 @@ export default function UserOrTruck(){
         <>
         <div className="flex gap-7">
 
-            <form onSubmit={CustomerRouter}>
-                <input type="submit" value="Are you a customer?" className="flex items-center text-7xl bg-red-500 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer"/>
-                    {/* <p className="text-8xl">Are you a customer?</p> */}
-            </form>
+            <div onClick={CustomerRouter} className="flex items-center text-7xl bg-linear-to-b from bg-red-300 via-red-600 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer">
+                <p className="text-8xl">Are you a customer?</p>
+           
+                <div className="absolute">
+                    <div className="relative">
+                        <User className="absolute scale-500 left-95 top-50"></User>
+                    </div>
+                </div>
+           
+            </div>
 
 
             <div className="flex justify-center items-center">
@@ -31,12 +43,38 @@ export default function UserOrTruck(){
             </div>
 
 
-            <form onSubmit={TruckRouter}>
-                <input type="submit" value="Are you a Food Truck?" className="flex items-center text-7xl bg-blue-500 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer"/>
-                    {/* <p className="text-8xl">Are you a Food Truck?</p> */}
+            <div onClick={TruckRouter} className="flex flex-wrap items-center text-7xl bg-linear-to-b from bg-blue-300 via-blue-600 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer">
+                <p className="text-8xl">Are you a Vendor?</p>
+            
+                <div className="absolute">
+                    <div className="relative">
+                        <Truck className="absolute scale-500 left-100 top-50"></Truck>
+                    </div>
+                </div>
                 
-            </form>
+            </div>
         </div>
         </>
     )
 }
+
+
+
+
+// This is another version using the submit button
+//   <form onSubmit={CustomerRouter}>
+//         <input type="submit" value="Are you a customer?" className="flex items-center text-7xl bg-linear-to-b from bg-red-300 via-red-600 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer"/>
+//             {/* <p className="text-8xl">Are you a customer?</p> */}
+//     </form>
+
+
+//     <div className="flex justify-center items-center">
+//         <p style={{WebkitTextStroke: "1px black"}} className="text-6xl font-semibold">OR</p>
+//     </div>
+
+
+//     <form onSubmit={TruckRouter}>
+//         <input type="submit" value="Are you a Vendor?" className="flex items-center text-7xl bg-linear-to-b from bg-blue-300 via-blue-600 w-full h-200 p-10 rounded-2xl hover:scale-105 hover:shadow-xl transition-all duration-300 shadow-2xl cursor-pointer"/>
+//             {/* <p className="text-8xl">Are you a Food Truck?</p> */}
+        
+//     </form>
