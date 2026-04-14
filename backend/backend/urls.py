c2065539ac199ae3b568ca2d94b2b8c2a578e612
Andrew Nieves <tun29995@temple.edu>
2026-04-13 23:15:39 -0400

    allowed urls.py to redirect to the DRF API page (localhost:8000/foodtrucks/)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import FoodTruckList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('foodtrucks/', FoodTruckList.as_view(), name='foodtruck-list'),
 ]
