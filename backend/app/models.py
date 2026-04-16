a928041ac1219a97b61f017fcf4634fb1bb3b538
Andrew Nieves <tun29995@temple.edu>
2026-04-16 12:20:30 -0400

    Created 'owner' model for truck owners

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,6 @@
 from django.contrib.gis.db import models
 from django.utils import timezone
+from django.conf import settings
 
 # Food truck model with fields for name, food type, location, and active status
 
@@ -15,6 +16,17 @@ class FoodTruck(models.Model):
     status = models.CharField(max_length=10, choices=statusChoices, default='OPEN') # Whether the food truck is currently open or closed
     openingTime = models.TimeField(null=True, blank=True) # Time when the food truck opens
     closingTime = models.TimeField(null=True, blank=True) # Time when the food truck closes
+
+    # Foreign key to the user model to associate a food truck with its owner
+    # For example, when a truck is created, the owner field will be set to the user who created it.
+    # In addition, the truck is associated with an ID number, which is used to identify it in the database and in API requests.
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='food_trucks',
+        null = True,
+        blank = True
+    )
 
     def __str__(self):
         return self.name
