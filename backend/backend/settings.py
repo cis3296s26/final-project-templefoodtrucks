ae531451d70b42f2d8add796521f12db92448c76
Andrew Nieves <tun29995@temple.edu>
2026-04-16 12:22:21 -0400

    Added library

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'rest_framework',
+    'rest_framework_gis',
     'corsheaders',
     "app" # if we end up changing the name of the app folder, change it here too
 ]
