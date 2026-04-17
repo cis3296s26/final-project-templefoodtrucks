bdaf0d655a0af7b7a960dca3320fae6e010e0bd1
Andrew Nieves <tun29995@temple.edu>
2026-04-17 12:06:42 -0400

    Updated auth urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,4 +28,8 @@ urlpatterns = [
     # Detail view for a specific food truck, identified by its primary key (pk)
     # for example, /foodtrucks/1/ would retrieve the details of the food truck with ID=1
     path('foodtrucks/<int:pk>/', views.FoodTruckDetail.as_view(), name='foodtruck-detail'),
+    # URL for verifying the signup token and creating a new user account
+    path('auth/verify-signup/', views.verify_signup, name='verify-signup'),
+    path('auth/generate-invite-link/', views.generate_invite_link, name='generate-invite-link'),
+    path('auth/verify-invite-and-signup/', views.verify_invite_and_signup, name='verify-invite-and-signup'),
 ]
